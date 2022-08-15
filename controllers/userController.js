23bc000d542038dde450ba9587a1a0e6e7ef5d12
Manuel Perera <manuel.perera@student.ha.dev>
2022-08-15 15:23:53 -0300

    merge main with show follower/following

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,63 +6,20 @@ async function index(req, res) {}
 
 // Display the specified resource.
 async function show(req, res) {
-	const users = await User.find().limit(3);
-	const user = await User.findOne({ userName: req.params.userName }).populate({
-		path: "tweetList",
-		populate: {
-			path: "author",
-		},
-	});
+	  const suggestedUsers = await User.find({
+      _id: { $nin: req.user.followingList },
+    });
 
-	return res.render("profilePage", { users, user });
-}
+	const user = await User.findOne({ userName: req.params.userName })
+    .populate({
+      path: "tweetList",
+      populate: {
+        path: "author",
+      },
+    })
+    .populate("followerList");
 
-async function follow(req, res) {
-	await User.findOneAndUpdate(
-		{ userName: req.params.userName },
-		{
-			$push: { followerList: req.user },
-		}
-	);
-
-	const user = await User.findOne({ userName: req.params.userName });
-	await User.findOneAndUpdate(
-		{ userName: req.user.userName },
-		{
-			$push: { followingList: user },
-		}
-	);
-	res.redirect("back");
-}
-
-async function unfollow(req, res) {
-	await User.findOneAndUpdate(
-		{ userName: req.params.userName },
-		{
-			$pull: { followerList: req.user._id },
-		}
-	);
-
-	const user = await User.findOne({ userName: req.params.userName });
-	await User.findOneAndUpdate(
-		{ userName: req.user.userName },
-		{
-			$pull: { followingList: user._id },
-		}
-	);
-	const suggestedUsers = await User.find({
-		_id: { $nin: req.user.followingList },
-	});
-
-	const authUser = req.user;
-	const user = await User.findOne({ userName: req.params.userName }).populate({
-		path: "tweetList",
-		populate: {
-			path: "author",
-		},
-	});
-
-	return res.render("profilePage", { suggestedUsers, user, authUser });
+	return res.render("profilePage", { suggestedUsers, user, authUser: req.user });
 }
 
 async function follow(req, res) {
@@ -99,15 +56,27 @@ async function unfollow(req, res) {
 }
 
 async function showFollowers(req, res) {
-	const followerUsers = await User.find({ $in: req.user.followerList });
-	const users = await User.find().limit(3);
-	return res.render("showFollowers", { followerUsers, users });
+	// const followerUsers = await User.find({ $in: req.user.id.followerList });
+  const user = await User.findOne({ _id: req.params.id }).populate("followerList");
+	const followerUsers = await User.find({_id: {$in: user.followerList}})
+	const suggestedUsers = await User.find({
+      _id: { $nin: req.user.followingList },
+    });
+
+	return res.render("showFollowers", { followerUsers, suggestedUsers });
 }
 
 async function showFollowing(req, res) {
-	const followingUsers = await User.find({ $in: req.user.followingList });
-	const users = await User.find().limit(3);
-	return res.render("showFollowing", { followingUsers, users });
+	// const followingUsers = await User.find({ $in: req.body.id.followingList });
+	const user = await User.findOne({ _id: req.params.id }).populate(
+    "followingList"
+  );
+  const followingUsers = await User.find({ _id: { $in: user.followingList } });
+	  const suggestedUsers = await User.find({
+      _id: { $nin: req.user.followingList },
+    });
+
+	return res.render("showFollowing", { followingUsers, suggestedUsers });
 }
 
 async function search(req, res) {
--- dbInitialSetup.js
+++ dbInitialSetup.js
@@ -4,7 +4,7 @@ const userSeeder = require("./seeders/userSeeder");
 
 module.exports = async () => {
 	db.connection;
-	await userSeeder();
-	await tweetSeeder();
+	// await userSeeder();
+	// await tweetSeeder();
 	console.log("[Database] Se han insertado los datos");
 };
